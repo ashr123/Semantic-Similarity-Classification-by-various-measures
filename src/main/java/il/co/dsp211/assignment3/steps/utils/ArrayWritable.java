797d8e953f90f2de8db385d526e78b5044335812
Amit Binenfeld <amitbin20@gmail.com>
2021-02-21 21:04:55 +0200

    Added our extended ArrayWritable.java

---
+++ src/main/java/il/co/dsp211/assignment3/steps/utils/ArrayWritable.java
@@ -0,0 +1,46 @@
+package il.co.dsp211.assignment3.steps.utils;
+
+import org.apache.hadoop.io.Writable;
+
+public class ArrayWritable extends org.apache.hadoop.io.ArrayWritable {
+
+	public ArrayWritable(Class<? extends Writable> valueClass) {
+		super(valueClass);
+	}
+
+	public ArrayWritable(Class<? extends Writable> valueClass, Writable[] values) {
+		super(valueClass, values);
+	}
+
+	public ArrayWritable(String[] strings) {
+		super(strings);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder stringBuilder = new StringBuilder();
+		arraysString(stringBuilder, super.get());
+		return stringBuilder.toString();
+	}
+
+	private static void arraysString(StringBuilder b, Object[] a)
+	{
+		if (a == null)
+		{
+			b.append("null");
+			return;
+		}
+
+		int iMax = a.length - 1;
+		if (iMax == -1)
+			return;
+
+		for (int i = 0; ; i++)
+		{
+			b.append(a[i]);
+			if (i == iMax)
+				return;
+			b.append("#");
+		}
+	}
+}
